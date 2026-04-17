b76ac342b0678aa9217fd57abada634af5f5e249
maestitia-x <maestitia0@gmail.com>
2026-04-17 17:48:50 +0300

    Validate todo Title middleware eklendi

---
+++ src/middlewares/validateTodoTitle.js
@@ -0,0 +1,13 @@
+import {AppError} from "../utils/AppError.js";
+
+export function validateTodoTitle(req,res,next){
+    const {title} = req.body
+    if (title === undefined) {
+        return(new AppError("Title must be exist!",400))
+    }
+
+    if (typeof title !== 'string') {
+        return next(new AppError("Title must be string! Not another data type",400))
+    }
+    next()
+}
--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -1,11 +1,12 @@
 import {Router} from "express";
 import {getTodos, getTodo, addTodo, editTodo, removeTodo} from "../controllers/todoControllers.js";
+import {validateTodoTitle} from "../middlewares/validateTodoTitle.js";
 
 const router = Router();
 
 router.get("/", getTodos);
 router.get("/:id", getTodo);
-router.post("/", addTodo);
+router.post("/",validateTodoTitle, addTodo);
 router.patch("/:id", editTodo);
 router.delete("/:id", removeTodo)
 
